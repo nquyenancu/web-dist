ec80b50ca145f7924da6abd367d0b5d8014690d6
Dev <4ever.kute2@gmail.com>
2023-06-30 09:25:15 +0700

    test

--- dist/assets/ImageWithError.3f2d8478.js
+++
@@ -1,1 +0,0 @@
-import{r as n,j as e,aj as o,m as s,y as c}from"./index.cc96c54e.js";const l=r=>{const[a,t]=n(!1);return e(c,{get when(){return!a()},get fallback(){return r.fallbackErr},get children(){return e(o,s(r,{onError:()=>{t(!0)}}))}})};export{l as I};
